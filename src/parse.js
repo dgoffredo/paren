66a3dc892e0ffaa5d0b4809df13f8909d7e25e41
David Goffredo <dmgoffredo@gmail.com>
2023-11-25 18:24:07 -0500

    simplification due to only one flavor of list

--- src/parse.js
+++ src/parse.js
@@ -27,40 +27,37 @@ Here are the resulting parse tree nodes:
 Note that whitespace tokens are ignored during parsing.
 */
 
-function listParser(tokens, index) {
+function parseList(tokens, index) {
     // The idea is to parse (`doParse`) elements until the parser returns
     // `undefined` as its parsed value. This will mean that we've encountered a
     // token for which the parser has no case, which means it's a closing
     // bracket/brace/parenthesis. Then do some error checking to make sure the
     // closing token is as expected, and finally return the array of parsed
     // elements and the following token index.
-
-    return () => {
-        const result = [];
-        var   parsed, tokenAfter;
-        for (;;) {
-            [parsed, index] = doParse(tokens, index);
-            if (parsed === undefined) {
-                break;
-            }
-
-            result.push(parsed);
-        };
-
-        if (index >= tokens.length) {
-            // assert.deepEqual(() => index,() => tokens.length);
-            throw Error(`Reached end without expected ")"`);
+    const result = [];
+    var   parsed, tokenAfter;
+    for (;;) {
+        [parsed, index] = doParse(tokens, index);
+        if (parsed === undefined) {
+            break;
         }
 
-        tokenAfter = tokens[index];
-        if (tokenAfter.text !== ')') {
-            throw Error('Mismatched grouping tokens. Expected a closing ' +
-                            `")" but found a ${tokenAfter.kind}: ` +
-                            tokenAfter.text);
-        }
-
-        return [{list: result}, index + 1];
+        result.push(parsed);
     };
+
+    if (index >= tokens.length) {
+        // assert.deepEqual(() => index,() => tokens.length);
+        throw Error(`Reached end without expected ")"`);
+    }
+
+    tokenAfter = tokens[index];
+    if (tokenAfter.text !== ')') {
+        throw Error('Mismatched grouping tokens. Expected a closing ' +
+                        `")" but found a ${tokenAfter.kind}: ` +
+                        tokenAfter.text);
+    }
+
+    return [{list: result}, index + 1];
 }
 
 function doParse(tokens, index) {
@@ -68,17 +65,17 @@ function doParse(tokens, index) {
         return;
     }
 
-    var token = tokens[index],
-        other = () => [undefined, index];
+    const token = tokens[index];
 
-    // switch on token kind
-    return ({
-        string: () => [{string: token.text, flavor: 'string'}, index + 1],
-        atString: () => [{string: token.text, flavor: 'atString'}, index + 1],
-        number: () => [{number: token.text}, index + 1],
-        symbol: () => [{symbol: token.text}, index + 1],
-        openParenthesis: listParser(tokens, index + 1),
-    }[token.kind] || other)();
+    switch (token.kind) {
+    case 'string': return [{string: token.text, flavor: 'string'}, index + 1];
+    case 'atString': return [{string: token.text, flavor: 'atString'}, index + 1];
+    case 'number': return [{number: token.text}, index + 1];
+    case 'symbol': return [{symbol: token.text}, index + 1];
+    case 'openParenthesis': return parseList(tokens, index + 1);
+    default:
+        return [undefined, index];
+    }
 }
 
 function parseOne(tokens) {
